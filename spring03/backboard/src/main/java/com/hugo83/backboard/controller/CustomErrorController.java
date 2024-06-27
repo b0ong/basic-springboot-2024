1f70fe6189bec5054f4f6b85aebc826605baacdd
Guk Jin Han <rnrwls2482@naver.com>
2024-06-27 09:49:37 +0900

    .

---
+++ spring03/backboard/src/main/java/com/hugo83/backboard/controller/CustomErrorController.java
@@ -0,0 +1,7 @@
+package com.hugo83.backboard.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+
+public class CustomErrorController implements ErrorController {
+
+}
